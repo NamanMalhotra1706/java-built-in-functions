5b9887a106e21daf9227837869bd87cf2f6eb339
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-07 14:55:53 +0530

    [Naman Malhotra] ADD: FiGCD and LCM Calculator

---
+++ GCDAndLCM.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GCDAndLCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        int gcd = calculateGCD(num1, num2);
+        int lcm = calculateLCM(num1, num2, gcd);
+        
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+    }
+
+    // Function to calculate GCD using the Euclidean algorithm
+    public static int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Function to calculate LCM
+    public static int calculateLCM(int a, int b, int gcd) {
+        return (a * b) / gcd;
+    }
+}
